87ebef5d881e90faacc7bebce99eb57b616ed7c1
NADESHIKON <allenzhang040817@gmail.com>
2022-07-31 13:20:51 -0700

    Another attempt on Rapid Cloud socket

--- src/socket/socket.service.ts
+++ src/socket/socket.service.ts
@@ -4,25 +4,31 @@ import WebSocket from 'ws';
 @Injectable()
 export default class SocketService implements OnModuleInit {
     public serverId: string;
+    private websocket: WebSocket;
 
     async onModuleInit() {
-        const ws = new WebSocket("wss://ws1.rapid-cloud.ru/socket.io/?EIO=4&transport=websocket");
+        this.websocket = new WebSocket("wss://ws1.rapid-cloud.ru/socket.io/?EIO=4&transport=websocket");
 
-        ws.on("open", () => {
-            ws.send("40");
+        this.websocket.on("open", () => {
+            this.websocket.send("40");
         });
 
-        ws.on("message", (data: string) => {
+        this.websocket.on("message", (data: string) => {
             data = data.toString();
             if (data?.startsWith("40")) {
                 this.serverId = JSON.parse(data.split("40")[1]).sid;
             } else if (data === "2") {
-                ws.send("3");
+                this.websocket.send("3");
+            } else if (data === "3") {
+                this.websocket.send("2");
+            } else {
+                this.websocket.send("40");
             }
         });
 
         setInterval(() => {
-            ws.send("3");
-        }, 1000);
+            this.websocket.send("2");
+            this.websocket.send("3");
+        }, 5000);
     }
 }
