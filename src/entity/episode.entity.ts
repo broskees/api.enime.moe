1864af752e80269b9e09de79110bc1970b021894
NADESHIKON <allenzhang040817@gmail.com>
2022-08-19 16:09:40 -0700

    [ci skip] Update docs

--- src/entity/episode.entity.ts
+++ src/entity/episode.entity.ts
@@ -28,10 +28,31 @@ export default class Episode {
     title: string | null;
 
     @ApiProperty({
-        description: "Episode title",
-        example: null
+        description: "Episode title variations",
+        example: {
+            english: "Easy Does it",
+            native: "Easy Does it"
+        }
+    })
+    titleVariations: object | null;
+
+    @ApiProperty({
+        description: "Episode description",
+        example: "Takina Inoue, a secret agent who protects the security of Japan called a “Lycoris,” is ordered to transfer to the café LycoReco in consequence of a certain incident. Takina is enthusiastic about returning to DA now that she’s buddied up with the super-elite No. 1 Lycoris (?) Chisato, but her job at LycoReco is just one weird thing after another!?"
+    })
+    description: string | null;
+
+    @ApiProperty({
+        description: "Episode image",
+        example: "https://artworks.thetvdb.com/banners/v4/episode/8916235/screencap/62c116db01f7f.jpg"
     })
     image: string | null;
+
+    @ApiProperty({
+        description: "Episode aired date",
+        example: "2022-07-02T07:00:00.000Z"
+    })
+    airedAt: Date | null;
 
     @ApiProperty({
         description: "Episode source IDs",
