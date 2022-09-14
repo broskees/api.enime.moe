18a2e097a276b771d90b958c64d0530b022eb42e
NADESHIKON <allenzhang040817@gmail.com>
2022-09-14 13:18:06 -0700

    Remove exposed rapid cloud key endpoint

--- src/tool/tool.controller.ts
+++ src/tool/tool.controller.ts
@@ -34,9 +34,4 @@ export default class ToolController {
     async rapidCloudServerId() {
         return this.rapidCloudService.serverId;
     }
-
-    @Get("/rapid-cloud/decryption-key")
-    async rapidCloudDecryptionKey() {
-        return this.rapidCloudService.decryptionKey;
-    }
 }
